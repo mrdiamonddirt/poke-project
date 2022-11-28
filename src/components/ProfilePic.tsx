f3086cbbe73f026d9c897c434f9a2d7f130205a1
RowDog <mrdiamonddirt@gmail.com>
2022-11-28 00:07:21 +0000

    fixed profile pic and data return from auth0

--- src/components/ProfilePic.tsx
+++ src/components/ProfilePic.tsx
@@ -1,14 +1,21 @@
-import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import styled from "styled-components";
 
 export const ProfileComponent = () => {
-  const { user } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
-    <div>
-      <img
-        src={user && user.picture}
-        alt="Profile"
-        className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-      />
-    </div>
+    isAuthenticated && (
+      <div>
+        <img style={{width: '150px'}} src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
     )
-}
+  );
+};
